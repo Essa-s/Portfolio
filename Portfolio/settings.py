13c42e9fcb211feee1a5758fe5c478ec072b5d0c
Essa <essa4a@gmail.com>
2024-01-30 14:38:42 +0200

    migrate

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') 
 
-ALLOWED_HOSTS = ['essa-saeed.onrender.com']
+ALLOWED_HOSTS = ['essa-saeed.onrender.com','127.0.0.1']
 
 
 # Application definition
